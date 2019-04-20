791b6f8c50006a560b98bfee34c29ade5d24baeb
Eliseu Macedo <eliseumcd@gmail.com>
2019-04-19 21:44:36 -0300

    Projeto criado

---
+++ src/entities/Employee.java
@@ -0,0 +1,49 @@
+package entities;
+
+public class Employee {
+	
+	private int id;
+	private String name;
+	private double salary;
+	
+	//Constructors
+	public Employee() {
+	}
+	
+	//metodo construtor
+	public Employee(int id, String name, double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+	
+	//get and set
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+	public void increaseSalary(double percent) {
+		salary += salary * (percent/100); 
+	}
+
+	@Override
+	public String toString() {
+		return id
+				+ ", "
+				+ name
+				+", " 
+				+ String.format("%.2f%n", salary);
+	}
+
+}
